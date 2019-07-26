b6dca1759370895f9ec77f382337e1590f210b9a
Damola Adewunmi <damolasd@gmail.com>
2019-07-26 10:30:57 +0100

    Added React Application with Login page

---
+++ sprint-auth-client/src/components/Login.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+import styled from 'styled-components';
+import { EventEmitter } from 'events';
+
+const StyledForm = styled.form`
+input {
+    padding: 10px;
+    margin: 10px;
+    border-radius: 10px;
+  }
+  button {
+    padding: 10px;
+    margin: 10px;
+    border-radius: 20px;
+    background-color: goldenrod;
+    cursor: pointer;
+  }
+`;
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password: '',
+            message: ''
+        }
+    }
+
+    login = event => {
+        event.preventDefault();
+
+        const newUser = {
+            username: this.state.username,
+            password: this.state.password
+        };
+
+        Axios.post('http://localhost:3300/api/login', newUser)
+        .then(res => {
+            
+            this.setState({
+              username: '',
+              password: '',
+              message: res
+            });
+    })
+};
+
+    handleInputChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+      };
+
+    render() {
+        return (
+            <StyledForm onSubmit={this.login}>
+                <input
+                    onChange={this.handleInputChange}
+                    placeholder="username"
+                    value={this.state.username}
+                    name="username"
+                />
+                <input
+                    onChange={this.handleInputChange}
+                    placeholder="password"
+                    value={this.state.password}
+                    name="password"
+                    type="password"
+                />
+
+                <div
+                    value={this.state.message}></div>
+                <button type="submit">Login</button>
+
+
+            </StyledForm>
+        )
+    }
+}
+
+
+export default Login;
